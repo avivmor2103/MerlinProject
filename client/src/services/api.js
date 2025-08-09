8295277eecc09885870a89356c1a385af1a5d05e
Aviv Mor <avivmore88@gmail.com>
2025-08-09 04:03:12 +0300

    Update API baseURL to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
+    baseURL: 'https://merlin-backend-7qpl.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
